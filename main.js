ff990dddc3c3f5efb10d301be7b37f50c1805ae8
Robert Strohmeyer <rstrohmeyer@demandbase.com>
2018-03-26 14:50:11 -0700

    added firebase

---
+++ main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBXMV0buglzYK8O1sihii-MJNS3yVVgh-k",
+  authDomain: "boozio-221e3.firebaseapp.com",
+  databaseURL: "https://boozio-221e3.firebaseio.com",
+  projectId: "boozio-221e3",
+  storageBucket: "",
+  messagingSenderId: "646890601299"
+};
+firebase.initializeApp(config);
