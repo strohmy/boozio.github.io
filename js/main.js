41bbab3dcc9ecc383662321bb936193ef58b57c7
Robert Strohmeyer <rstrohmeyer@demandbase.com>
2018-04-09 17:01:35 -0700

    added error handling on db query

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ firebase.initializeApp(
 
 // Get a reference to the database service
 var db = firebase.firestore();
+drinksDB = db.collection('drinks');
 
 var drinks = [];
 
@@ -21,28 +22,51 @@ $(window).on('load', function() {
 
 function getDrinks() {
   // Listen for changes in drinks data
-  db.collection('drinks').orderBy('likes', 'desc').get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      $('#drinksList').append(`
-        <div class='drinkDiv'>
-          <div>
-            <div class='thumbAndLikeBox'>
-              <img class='drinkThumbnail' src='img/martini.jpg'><br/>
-              <p align='center'><i class='far fa-heart'></i> ${doc.data().likes}</p>
-            </div>
-            <h3>${doc.data().name}</h3>
-            <p class='drinkDescription'>${doc.data().description}</p>
-            <h4 class='ingredientsSubhed'>Ingredients</h4>
-            <ul id='ingredientsList' class='ingredientsList'>
-              ${doc.data().ingredients.join('<br />')}
-            </ul>
-            <p class='drinkInstructions'>${doc.data().instructions}</p>
+  drinksDB.orderBy('likes', 'desc').get().then((querySnapshot) => {
+    var heartIterator = 0;
+    if (querySnapshot.doc) {
+      querySnapshot.forEach((doc) => {
+        $('#drinksList').append(`
+          <div class='drinkDiv' id='${doc.id}'>
+              <div class='thumbAndLikeBox'>
+                <img class='drinkThumbnail' src='img/martini.jpg'><br/>
+                <p align='center'><i id='heartIcon-${heartIterator}' class='heart far fa-heart'></i> ${doc.data().likes}</p>
+              </div>
+              <h3>${doc.data().name}</h3>
+              <p class='drinkDescription'>${doc.data().description}</p>
+              <h4 class='ingredientsSubhed'>Ingredients</h4>
+              <ul id='ingredientsList' class='ingredientsList'>
+                ${doc.data().ingredients.join('<br />')}
+              </ul>
+              <p class='drinkInstructions'>${doc.data().instructions}</p>
           </div>
-        </div>
-      `);
-    });
+        `);
+        heartIterator += 1;
+      })
+    } else {
+      $('#drinksList').append('Error loading drinks...')
+    }
   });
 }
+
+$('#drinksList').on('click', '.heart', function() {
+  var gotID = $(this).closest('.drinkDiv').attr('id');
+  var currentRecord = drinksDB.doc(gotID);
+  currentRecord.get().then(function(doc) {
+    if (doc.exists) {
+        var currentLikes = doc.data().likes;
+        currentLikes += 1;
+        currentRecord.set({
+          likes: currentLikes
+        });
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+  }).catch(function(error) {
+      console.log("Error getting document:", error);
+  });
+});
 
 $('#addIngredientButton').on('click', function() {
   $('#drinkIngredientSpan').append("<input type='text' placeholder='Enter another ingredient'>");
